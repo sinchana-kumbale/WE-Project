9a659ab42a32948913acafb3769cf47e472ae701
sinchana-kumbale <77568895+sinchana-kumbale@users.noreply.github.com>
2021-10-02 16:34:33 +0530

    Update RegisterScreen.js

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -29,7 +29,7 @@ export default function RegisterScreen({ navigation }) {
     }
     navigation.reset({
       index: 0,
-      routes: [{ name: 'Dashboard' }],
+      routes: [{ name: 'Dashboard', params:{user: email.value,username: 'sinch'}}],
     })
   }
 
